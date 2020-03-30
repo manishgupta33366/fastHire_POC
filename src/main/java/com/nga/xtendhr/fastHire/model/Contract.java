3ab8fdbd4803ece6af2f26277d8d3459bd325b98
ManishG <ManishG@HYD01LCO3423.user.ngahr.int>
2020-03-30 14:22:46 +0530

    initial commit

---
+++ src/main/java/com/nga/xtendhr/fastHire/model/Contract.java
@@ -0,0 +1,35 @@
+package com.nga.xtendhr.fastHire.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "\"com.nga.poc.fasthire.db::Table.FHD_CONTRACT\"", schema = "POC_FAST_HIRE")
+public class Contract {
+
+	@Id
+	@Column(name = "\"ID\"", columnDefinition = "VARCHAR(5000)")
+	private String id;
+
+	@Column(name = "\"TEMPLATE\"", columnDefinition = "VARCHAR(132)")
+	private String template;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+}
